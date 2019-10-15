5f9aed9e4113669882ab96ecddcd5c326ca509fb
Artem.Belikov <artsiom.belikov@gmail.com>
2019-10-15 14:56:10 +0300

    Branch

--- Leetcode/Tasks/BrightBoxTask.cs
+++ Leetcode/Tasks/BrightBoxTask.cs
@@ -1,7 +1,13 @@
+using System.Collections.Generic;
+using System.Reflection;
+
 namespace Leetcode.Tasks
 {
     public class BrightBoxTask
     {
-        
+        public static Dictionary<string, float> ParseDictionary(Dictionary<string, object> input)
+        {
+            return new Dictionary<string, float>();
+        } 
     }
 }
